a38e28018495fe9ef3032225513aca55e4e7fc96
pratikdaigavane <daigavanep@gmail.com>
2020-12-10 20:31:39 +0530

    bug fix

--- app/app/urls.py
+++ app/app/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     url(r'^auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'^auth/', include('djoser.urls.jwt')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
